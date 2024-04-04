48dcd98c0fbff6cb79f560bd1e3b3e6777a4eaa5
matteocenci <matteocenci01@gmail.com>
2024-04-04 18:06:26 +0200

    feat: pari and dispari

---
+++ ParieDispari/js/function1.js
@@ -0,0 +1,9 @@
+
+function isPari(sumNumbers) {
+    if (sumNumbers / 2 !== 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
--- ParieDispari/js/script1.js
+++ ParieDispari/js/script1.js
@@ -1,1 +1,9 @@
-let 
+let firstNumber = parseInt(prompt("Dammi un numero da 1 a 5: "));
+console.log(firstNumber);
+let secondNumber = parseInt(prompt("dammi un numero da 1 a 5: "));
+console.log(secondNumber);
+
+const sumNumbers = firstNumber + secondNumber;
+console.log(sumNumbers);
+
+console.log(isPari);
